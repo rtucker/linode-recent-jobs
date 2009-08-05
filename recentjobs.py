2da269a8006426632f431971d206a08f73ffeae2
Ryan Tucker <rtucker@gmail.com>
2009-08-04 20:11:25 -0400

    initial commit

---
+++ recentjobs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# Uses the Linode API to display recent jobs for your Linode.
+# Useful for running on boot to see why your Linode rebooted.
+
+# Needs api.py somewhere nearby.
+
+# Ryan Tucker <rtucker@gmail.com>, 2009/08/04
+
+import api
+import secrets
+import sys
+
+instance = api.Api(key=secrets.API_KEY)
+
+jobs = instance.linode_job_list(linodeid=secrets.LINODE_ID)
+
+if jobs:
+    sys.stdout.write('%8s %16s %16s %s %s\n' % ('JobID', 'Submit', 'Finish', '?', 'Description'))
+    sys.stdout.write('-'*79 + '\n')
+    for i in jobs:
+        sys.stdout.write('%8i %16s %16s %i %s\n' % (i['JOBID'], i['ENTERED_DT'][:16], i['HOST_FINISH_DT'][:16], i['HOST_SUCCESS'], i['LABEL']))
+else:
+    sys.stdout.write('No jobs detected for LinodeID %i' % secrets.LINODE_ID)
+
